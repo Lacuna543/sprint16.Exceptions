784143cc114a700b18841d8e17e2d50cd6aa4db4
nkvICEHW <slotvinskiy@gmail.com>
2020-08-04 19:16:30 +0300

    some commit

--- src/main/java/com/softserve/edu/exception/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/edu/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.softserve.edu.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,16 +9,20 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public String handlerPageNotFoundException() {
-        return "/error/404";
-    }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ModelAndView handleMyCustomException(EntityNotFoundException exception) {
-//        ModelAndView model = new ModelAndView("/error/error_page");
+    public ModelAndView handleMyCustomException(Exception exception) {
         ModelAndView model = new ModelAndView("error_page");
         model.addObject("info", exception.getMessage());
+        model.setStatus(HttpStatus.BAD_REQUEST);
+        return model;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ModelAndView handleMyCustomException(DataIntegrityViolationException exception) {
+        ModelAndView model = new ModelAndView("error_page");
+        String detail = exception.getMostSpecificCause().toString();
+        model.addObject("info", detail.substring(detail.indexOf("Подробности:")));
         model.setStatus(HttpStatus.BAD_REQUEST);
         return model;
     }
