55a53c92abce367bb7f0cf244ce668cea028041e
Lacuna543 <[kusunya@i.ua]>
2020-08-05 10:37:56 +0300

    Added handling PSQLException

--- src/main/java/com/softserve/edu/controller/CustomErrorController.java
+++ src/main/java/com/softserve/edu/controller/CustomErrorController.java
@@ -2,14 +2,26 @@ package com.softserve.edu.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CustomErrorController implements ErrorController {
+
+//    @ResponseBody
+//    @RequestMapping(value = "img/404.jpg", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.IMAGE_JPEG_VALUE)
+//    public ResponseEntity<byte[]> getImage(@PathVariable("id")String id) {
+//        byte[] image = imageService.getImage(id);  //this just gets the data from a database
+//        return ResponseEntity.ok(image);
+//    }
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
